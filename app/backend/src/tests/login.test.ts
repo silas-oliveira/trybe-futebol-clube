9182d718c5f32871865ac0b72938a392149b8db2
Armin1-lab <silas.oliveira8@gmail.com>
2022-03-27 22:56:11 -0300

    reqs testes iniciais

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,45 @@
+import * as mocha from 'mocha';
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+import { Response } from 'superagent';
+
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const USER_RETURN =	{
+  id: 2,
+  username: "User",
+  role: "user",
+  email: "user@user.com"
+}
+
+describe('Seu teste', () => {
+
+  let chaiHttpResponse: Response;
+  
+  before(async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({ email: 'user@user.com', password: 'secret_user' });
+  });
+
+  it('Espera que o status seja 200', async () => {
+    expect(chaiHttpResponse).to.have.status(200);
+  });
+
+  it('Espera que tenha o body possua as propriedades "user" e "token":', async () => {
+    expect(chaiHttpResponse.body).to.have.property('user');
+    expect(chaiHttpResponse.body).to.have.property('token');
+  });
+
+  it('Espera que o retorno de "user" seja igual ao esperado:', async () => {
+    expect(chaiHttpResponse.body.user).to.deep.equal(USER_RETURN);
+  });
+  
+});
